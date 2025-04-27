fb4959de32fe6c7cdbc30bf339cc31c334e119f2
dwan6767 <145912838+dwan6767@users.noreply.github.com>
2025-04-27 22:41:48 +0530

    Update puny.c

--- src/puny.c
+++ src/puny.c
@@ -2,8 +2,13 @@
 
 uint16_t Baud = 104;
 static uint16_t baud_delay = 0;
+void setupTimer0() {
+    TCCR0A = 0; 
+    TCCR0B = (1 << CS00); 
+}
 
 void puny_show_speed(uint32_t rate) {
+    setupTimer0();
     Baud = (F_CPU / 16 / rate) - 1;
     baud_delay = (Baud);
 }
@@ -16,29 +21,33 @@ void puny_wakeup() {
 
 void puny_throw(uint8_t data) {
     Tx_PORT &= ~(1 << Tx_BIT);  // Start bit
-    delayMicroseconds(baud_delay);  // Change _delay_us to delayMicroseconds
+    delay_us(baud_delay);
+    
     for (uint8_t b = 0; b < 8; b++) {
         if (data & (1 << b))
             Tx_PORT |= (1 << Tx_BIT);
         else
             Tx_PORT &= ~(1 << Tx_BIT);
         
-        delayMicroseconds(baud_delay);  // Change _delay_us to delayMicroseconds
+        delay_us(baud_delay);
     }
+
     Tx_PORT |= (1 << Tx_BIT);  // Stop bit
-    delayMicroseconds(baud_delay);  // Change _delay_us to delayMicroseconds
+    delay_us(baud_delay);
 }
 
 uint8_t puny_got() {
     uint8_t data = 0;
     while (Rx_STATE & (1 << Rx_BIT));  // Wait for start bit
-    delayMicroseconds(3 * baud_delay / 2);  // Center of first data bit
+    delay_us(3 * baud_delay / 2);  // Center of first data bit
+    
     for (uint8_t b = 0; b < 8; b++) {
         if (Rx_STATE & (1 << Rx_BIT))
             data |= (1 << b);
-        delayMicroseconds(baud_delay);  // Change _delay_us to delayMicroseconds
+        delay_us(baud_delay);
     }
-    delayMicroseconds(baud_delay);  // Stop bit
+    
+    delay_us(baud_delay);  // Stop bit
     return data;
 }
 
@@ -51,6 +60,14 @@ void puny_sentence(const char *str) {
 
 void puny_numbers(int number) {
     char buffer[10];
-    itoa(number, buffer, 10);
-    puny_sentence(buffer);
+    itoa(number, buffer, 10); 
+    puny_sentence(buffer);  
+}
+void delay_us(uint16_t us) {
+    uint16_t count = 0;
+    while (count < us) {
+        while (!(TIFR0 & (1 << TOV0)));  
+        TIFR0 |= (1 << TOV0);  
+        count++;
+    }
 }
