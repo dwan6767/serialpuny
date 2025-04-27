24f71ad3864c4637b1a8ce36bdb49d94cbbeaebc
dwan6767 <145912838+dwan6767@users.noreply.github.com>
2025-04-27 21:50:28 +0530

    Create puny.h

---
+++ src/puny.h
@@ -0,0 +1,38 @@
+#ifndef PUNY_H
+#define PUNY_H
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdlib.h>
+#ifndef F_CPU
+#define F_CPU 8000000UL 
+#endif
+
+
+#define Rx_DDR DDRB
+#define Tx_DDR DDRB
+#define Tx_PORT PORTB
+#define Rx_STATE PINB
+#define Rx_BIT PB1
+#define Tx_BIT PB3
+
+extern uint16_t Baud; 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+// Function declarations
+void puny_wakeup(void);
+void puny_throw(uint8_t data);
+uint8_t puny_got(void);
+void puny_show_speed(uint32_t rate);
+void puny_sentence(const char *str);
+void puny_numbers(int number);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif // PUNY_H
+
