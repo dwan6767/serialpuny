8b2d567e8e1a9c2bf9f45924d9ebd71e8ac39ad6
dwan6767 <145912838+dwan6767@users.noreply.github.com>
2025-04-27 22:45:51 +0530

    Update puny.c

--- src/puny.c
+++ src/puny.c
@@ -2,13 +2,8 @@
 
 uint16_t Baud = 104;
 static uint16_t baud_delay = 0;
-void setupTimer0() {
-    TCCR0A = 0; 
-    TCCR0B = (1 << CS00); 
-}
 
 void puny_show_speed(uint32_t rate) {
-    setupTimer0();
     Baud = (F_CPU / 16 / rate) - 1;
     baud_delay = (Baud);
 }
@@ -21,33 +16,29 @@ void puny_wakeup() {
 
 void puny_throw(uint8_t data) {
     Tx_PORT &= ~(1 << Tx_BIT);  // Start bit
-    delay_us(baud_delay);
-    
+    delayMicroseconds(baud_delay);  // Change _delay_us to delayMicroseconds
     for (uint8_t b = 0; b < 8; b++) {
         if (data & (1 << b))
             Tx_PORT |= (1 << Tx_BIT);
         else
             Tx_PORT &= ~(1 << Tx_BIT);
         
-        delay_us(baud_delay);
+        delayMicroseconds(baud_delay);  // Change _delay_us to delayMicroseconds
     }
-
     Tx_PORT |= (1 << Tx_BIT);  // Stop bit
-    delay_us(baud_delay);
+    delayMicroseconds(baud_delay);  // Change _delay_us to delayMicroseconds
 }
 
 uint8_t puny_got() {
     uint8_t data = 0;
     while (Rx_STATE & (1 << Rx_BIT));  // Wait for start bit
-    delay_us(3 * baud_delay / 2);  // Center of first data bit
-    
+    delayMicroseconds(3 * baud_delay / 2);  // Center of first data bit
     for (uint8_t b = 0; b < 8; b++) {
         if (Rx_STATE & (1 << Rx_BIT))
             data |= (1 << b);
-        delay_us(baud_delay);
+        delayMicroseconds(baud_delay);  // Change _delay_us to delayMicroseconds
     }
-    
-    delay_us(baud_delay);  // Stop bit
+    delayMicroseconds(baud_delay);  // Stop bit
     return data;
 }
 
@@ -60,14 +51,6 @@ void puny_sentence(const char *str) {
 
 void puny_numbers(int number) {
     char buffer[10];
-    itoa(number, buffer, 10); 
-    puny_sentence(buffer);  
-}
-void delay_us(uint16_t us) {
-    uint16_t count = 0;
-    while (count < us) {
-        while (!(TIFR0 & (1 << TOV0)));  
-        TIFR0 |= (1 << TOV0);  
-        count++;
-    }
+    itoa(number, buffer, 10);
+    puny_sentence(buffer);
 }
